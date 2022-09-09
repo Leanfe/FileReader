fea02e7694e56c9174dbec039a19bdc30be7617f
Ulynaxe <l.kapitman@gmail.com>
2022-09-09 14:48:18 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 #include "fileReader.h"
@@ -7,53 +8,46 @@
 int main(int argc, char *argv[])
 {
     int opt;
-    char* filename = NULL;
-    char* outfile = NULL;
+    FILE* filename = stdin;
+    FILE* outfile = stdout;
     int useOut = 0;
 
-    // поместите ':' в начало строки
-    //, чтобы программа могла
-    //различать '?' и ':'
-    while((opt = getopt(argc, argv, ":f:o:lrx")) != -1)
+    char* USAGE[] = {
+            "USAGE: fileReader [OPTION]\n",
+            "Concatenate FILE(s) to standard output. \n",
+            "\n",
+            "-f                       read from file \n",
+            "-o                       output to file \n",
+            "\n",
+            "Examples: \n",
+            "fileReader -f obeme.txt -o output.txt \n",
+    };
+
+    while((opt = getopt(argc, argv, ":f:o:h")) != -1)
     {
         switch(opt)
         {
             case 'f':
-                filename = optarg;
-                break;
-            case 'l':
-            case 'r':
-                printf("option: %c\n", opt);
+                filename = fopen(optarg, "r");
                 break;
             case 'o':
-                outfile = optarg;
-                useOut = 1;
+                outfile = fopen(optarg, "w");
                 break;
+            case 'h':
+                printf(USAGE);
+                return EXIT_SUCCESS;
             case ':':
-                printf("option needs a value\n");
-                break;
+                perror("Option needs a value!");
+                return EXIT_FAILURE;
             case '?':
-                printf("unknown option: %c\n", optopt);
+                perror("Unknown option!");
+                return EXIT_FAILURE;
+            default:
                 break;
         }
     }
 
-    if (filename == NULL) {
-        printf("не указан файл откуда читать! \n");
-        return 1;
-    }
-
-    // опция предназначена для дополнительных аргументов
-    // которые не анализируются
-    for(; optind < argc; optind++){
-        printf("extra arguments: %s\n", argv[optind]);
-    }
-
-    if (useOut == 1) {
-        printToFile(readFile(filename), outfile);
-    } else {
-        printArray(readFile(filename));
-    }
-    return 0;
+    //printArray(readFile(filename, outfile));
+    return EXIT_SUCCESS;
 }
 
